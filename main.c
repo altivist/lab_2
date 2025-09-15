a964ca2824efd24aee9e749a9ea43df14c53529c
altivist <megattmail@gmail.com>
2025-09-15 14:43:03 +0300

    Пока без блоков

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+    
+int main()
+{
+    const char *file1 = "file1.txt", *file2 = "file2.txt", *file3 = "file3.txt";
+    FILE *f1 = fopen(file1, "w"), *f2 = fopen(file2, "w"), *f3 = fopen(file3, "w");
+
+    if (f1 == NULL || f2 == NULL || f3 == NULL)
+    {
+        printf("Не удалось создать файлы\n");
+        return 1;
+    }
+
+    srand(time(NULL));
+
+    for (int i = 0; i < 10; i++)
+    {
+        int num1 = (rand() % 10) + 1;
+        int num2 = (rand() % 10) + 1;
+        int num3 = (rand() % 10) + 1;
+
+        fprintf(f1, "%d ", num1);
+        fprintf(f2, "%d ", num2);
+        fprintf(f3, "%d ", num3);
+    }
+
+    fclose(f1);
+    fclose(f2);
+    fclose(f3);
+
+    f1 = f2 = f3 = NULL;
+    return 0;
+}
